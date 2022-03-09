70079dccd953964687a4b22cb299b7e3a9bfc086
Tin Nguyen <hopthucuatin@gmail.com>
2022-03-09 22:58:46 +0700

    Add campaign by user (contract and frontend)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,105 +1,120 @@
+import { ViewListIcon } from "@heroicons/react/solid";
+
 interface IProfileProptypes {
-    avatar?: string | null;
-    displayName: string;
-    desc: string;
-    email: string;
-    href: string;
-    jobName: string;
-    bgImage: string;
-    userType: string;
+  avatar?: string | null;
+  displayName: string;
+  desc: string;
+  email: string;
+  href: string;
+  jobName: string;
+  bgImage: string;
+  userType: string;
+  campaignsDonated: any;
 }
 
 export default function Profile({
-    avatar,
-    displayName,
-    desc,
-    email,
-    href,
-    jobName,
-    bgImage,
-    userType
+  avatar,
+  displayName,
+  desc,
+  email,
+  href,
+  jobName,
+  bgImage,
+  userType,
+  campaignsDonated,
 }: IProfileProptypes): JSX.Element {
-    return (<>
-        {/* ! ------------------------------------------------------------
+  return (
+    <>
+      {/* ! ------------------------------------------------------------
         ! Profile banner and avatar
         ! ------------------------------------------------------------ */}
-        <div className="w-full">
-            <div className="w-full h-48 bg-gray-600 rounded-t-lg">
-                <img src={bgImage} className="object-cover w-full h-full" />
-
-            </div>
-            <div className="absolute ml-5 -mt-20">
-                <div className={"w-40 bg-gray-200 border border-b border-gray-300 rounded-lg shadow-md h-36 border-primary"}>
-                    {avatar &&
-                        <img src={avatar} className="object-cover w-full h-full" />
-                    }
-                </div>
-            </div>
+      <div className="w-full">
+        <div className="w-full h-48 bg-gray-600 rounded-t-lg">
+          <img src={bgImage} className="object-cover w-full h-full" />
         </div>
+        <div className="absolute ml-5 -mt-20">
+          <div
+            className={
+              "w-40 bg-gray-200 border border-b border-gray-300 rounded-lg shadow-md h-36 border-primary"
+            }
+          >
+            {avatar && (
+              <img src={avatar} className="object-cover w-full h-full" />
+            )}
+          </div>
+        </div>
+      </div>
 
-        {/* ! ------------------------------------------------------------
+      {/* ! ------------------------------------------------------------
         ! Profile general information
         ! ------------------------------------------------------------ */}
-        <div className="flex flex-col p-5 pt-20 text-black border rounded-b-lg bg-neutral-300 bg-primary border-primary">
-            <div className="w-40 text-2xl">
-                {displayName || window.accountId}
-            </div>
-            <div className="mb-1 text-sm font-light w-96">
-                {displayName && (
-                    <>
-                        {window.accountId} | {userType}
-                    </>
-                )}
-            </div>
-            <div className="mt-2 text-sm">
-                <div className="flex flex-row items-center ml-auto space-x-2">
-                    <div className="w-full h-5 mb-1 ">
-                        {desc && (
-                            <>
-                                <b>
-                                    Bio:
-                                </b><br />
-                                {desc}
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex gap-8 pt-8">
-                <div className="flex flex-col">
-                    <div className="w-20 h-5 mb-1 text-2xl">
-                        100
-                    </div>
-                    <div className="w-32 h-5 mb-1 text-sm">
-                        Projects donated
-                    </div>
-                </div>
-                <div className="flex flex-col">
-                    <div className="w-20 h-5 mb-1 text-2xl">
-                        200
-                    </div>
-                    <div className="w-32 h-5 mb-1 text-sm">
-                        Money donated
-                    </div>
-                </div>
-                <div className="flex flex-col">
-                    <div className="w-20 h-5 mb-1 text-2xl">300</div>
-                    <div className="w-32 h-5 mb-1 text-sm">
-                        Project voted
-                    </div>
-                </div>
-                <div className="flex flex-col">
-                    <div className="w-20 h-5 mb-1 text-2xl">400</div>
-                    <div className="w-32 h-5 mb-1 text-sm">
-                        Project liked
-                    </div>
-                </div>
-            </div>
-            <div className="py-5 break-all bbcode">
-                <div className="h-5 mb-1 bg-gray-200 border border-gray-300 animate-pulse w-44"></div>
-                <div className="w-full h-40 mb-1 bg-gray-200 border border-gray-300 animate-pulse"></div>
-            </div>
+      <div className="flex flex-col p-5 pt-20 text-black border rounded-b-lg bg-neutral-300 bg-primary border-primary">
+        <div className="w-40 text-2xl">{displayName || window.accountId}</div>
+        <div className="mb-1 text-sm font-light w-96">
+          {displayName && (
+            <>
+              {window.accountId} | {userType}
+            </>
+          )}
         </div>
-    </>)
+        <div className="mt-2 text-sm">
+          <div className="flex flex-row items-center ml-auto space-x-2">
+            <div className="w-full h-5 mb-1 ">
+              {desc && (
+                <>
+                  <b>Bio:</b>
+                  <br />
+                  {desc}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-8 pt-8">
+          <div className="flex flex-col">
+            <div className="w-32 h-5 mb-1 text-2xl">
+              {campaignsDonated.length}
+            </div>
+            <div className="w-32 h-5 mb-1 text-sm">Campaigns donated</div>
+          </div>
+          <div className="flex flex-col">
+            <div className="w-20 h-5 mb-1 text-2xl">x</div>
+            <div className="w-32 h-5 mb-1 text-sm">Ⓝ donated</div>
+          </div>
+          <div className="flex flex-col">
+            <div className="w-20 h-5 mb-1 text-2xl">x</div>
+            <div className="w-32 h-5 mb-1 text-sm">Project voted</div>
+          </div>
+          <div className="flex flex-col">
+            <div className="w-20 h-5 mb-1 text-2xl">x</div>
+            <div className="w-32 h-5 mb-1 text-sm">Project liked</div>
+          </div>
+        </div>
+        <div className="py-5 break-all bbcode">
+          <div className="mb-1 font-medium text-xl flex items-center gap-1">
+            <ViewListIcon className="w-4 h-4" />
+            Donated campaigns:
+          </div>
+          <div className="w-full h-40 mb-1">
+            {campaignsDonated &&
+              campaignsDonated.map((campaign: any) => (
+                <div className="">
+                  <a
+                    //replace special characters or vietnamese character to url friendly
+                    href={`/single-campaign/${campaign.title.replace(
+                      /[^a-zA-Z0-9]/g,
+                      "-"
+                    )}/${campaign.campaign_id}`}
+                    className="hover:text-pink-500"
+                  >
+                    {campaign.title}
+                  </a>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,53 +1,80 @@
-import Link from 'next/link';
-import { TagIcon } from '@heroicons/react/outline';
-import React, { useState } from 'react';
+import Link from "next/link";
+import { TagIcon } from "@heroicons/react/outline";
+import React, { useEffect, useState } from "react";
 import ProfileLayout from "../../components/profile/ProfileLayout";
-import Profile from '../../components/Profile';
-import { useAppSelector } from '../../app/hooks';
-import { selectLoginState, selectUserState } from '../../app/login/login';
-import { NearAuthorType } from '../../data/types';
-import axios from 'axios';
-import { Widget } from '@uploadcare/react-widget';
-import ButtonPrimary from '../../components/ButtonPrimary';
-import { toast } from 'react-toastify';
+import Profile from "../../components/Profile";
+import { useAppSelector } from "../../app/hooks";
+import { selectLoginState, selectUserState } from "../../app/login/login";
+import { NearAuthorType } from "../../data/types";
+import axios from "axios";
+import { Widget } from "@uploadcare/react-widget";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 export default function ProfilePage() {
-    const loginState = useAppSelector(selectLoginState);
-    const userState = useAppSelector(selectUserState);
+  const router = useRouter();
+  const loginState = useAppSelector(selectLoginState);
+  const userState = useAppSelector(selectUserState);
+  const [campaigns, setCampaigns] = useState<any>();
 
-    const { displayName, avatar, desc, email, href, jobName, bgImage, type: userType }: any = userState || {};
+  const {
+    displayName,
+    avatar,
+    desc,
+    email,
+    href,
+    jobName,
+    bgImage,
+    type: userType,
+  }: any = userState || {};
 
+  useEffect(() => {
+    const getUserCampaigns = async () => {
+      if (!window.contract) {
+        return;
+      }
 
-    // const { avatar } = userState;
+      const campaigns = await window.contract.get_campaigns_by_user_paging({
+        account_id: window.accountId,
+        page_number: (router.query.page as string) || "1",
+        page_size: "10",
+      });
 
-    // const [avatar, setAvatar] = useState(null);
+      setCampaigns(campaigns);
+    };
+    getUserCampaigns();
+  }, [router.query.page, window.contract]);
 
-    // const onAvatarChange: any = (e: any) => {
-    //     e.preventDefault();
+  // const { avatar } = userState;
 
-    //     axios
-    //         .put("/api/user/change_avatar", { avatarUrl: avatar })
-    //         .then((response) => {
-    //             const { data } = response;
-    //             if (data.success) {
-    //                 toast.success("Thay avatar thành công");
-    //             } else {
-    //                 toast.error("Thay avatar không thành công");
-    //             }
-    //         });
-    // };
+  // const [avatar, setAvatar] = useState(null);
 
-    // const uploadToClient = (e: any) => {
-    //     console.log(e);
-    //     setAvatar(e.originalUrl);
-    //     // setCreateObjectURL(URL.createObjectURL(e.originalUrl));
-    // };
+  // const onAvatarChange: any = (e: any) => {
+  //     e.preventDefault();
 
+  //     axios
+  //         .put("/api/user/change_avatar", { avatarUrl: avatar })
+  //         .then((response) => {
+  //             const { data } = response;
+  //             if (data.success) {
+  //                 toast.success("Thay avatar thành công");
+  //             } else {
+  //                 toast.error("Thay avatar không thành công");
+  //             }
+  //         });
+  // };
 
-    return (
-        <>
-            <ProfileLayout>
-                {/* <div className="flex flex-col items-center justify-center p-4">
+  // const uploadToClient = (e: any) => {
+  //     console.log(e);
+  //     setAvatar(e.originalUrl);
+  //     // setCreateObjectURL(URL.createObjectURL(e.originalUrl));
+  // };
+
+  return (
+    <>
+      <ProfileLayout>
+        {/* <div className="flex flex-col items-center justify-center p-4">
                     {avatar && <img src={avatar} className="mb-3 w-96" />}
                     <Widget
                         publicKey="533d4b8f6a11de77ba81"
@@ -61,22 +88,18 @@ export default function ProfilePage() {
                     )}
                 </div> */}
 
-
-                <Profile
-                    avatar={avatar || "/images/no-avatar.png"}
-                    displayName={displayName}
-                    desc={desc}
-                    email={email}
-                    href={href}
-                    jobName={jobName}
-                    bgImage={bgImage}
-                    userType={userType}
-
-                />
-
-            </ProfileLayout>
-        </>
-    )
+        <Profile
+          avatar={avatar || "/images/no-avatar.png"}
+          displayName={displayName}
+          desc={desc}
+          email={email}
+          href={href}
+          jobName={jobName}
+          bgImage={bgImage}
+          userType={userType}
+          campaignsDonated={campaigns}
+        />
+      </ProfileLayout>
+    </>
+  );
 }
-
-
